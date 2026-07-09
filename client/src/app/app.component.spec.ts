6badfdc43b898932ed287b911fdba1af1ada8e22
Amanda <aruff@my365.bellevue.edu>
2026-07-08 18:18:38 -0600

    Fix AppComponent router test

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { routes } from './app.routes';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent]
+      imports: [AppComponent],
+      providers: [provideRouter(routes)]
     }).compileComponents();
   });
 
