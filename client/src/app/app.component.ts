ca5a4d304b94f462d6809f9b550a3b196c229bf5
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2026-07-22 23:33:51 -0400

    fix(client): redirect to landing page on logout instead of login
    Closes #20

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -172,6 +172,6 @@ export class AppComponent {
   logout(): void {
     this.authService.logout();
     this.isUserMenuOpen = false;
-    this.router.navigate(['/login']);
+    this.router.navigate(['/']);
   }
 }
